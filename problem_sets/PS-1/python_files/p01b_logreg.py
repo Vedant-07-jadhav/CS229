c3a200b9940c5149006ada429bd3c7d23fc973c9
Vedant <vedantdjadhav712@gmail.com>
2025-06-25 18:35:11 +0530

    Problem set 1

--- problem_sets/PS-1/python_files/p01b_logreg.py
+++ problem_sets/PS-1/python_files/p01b_logreg.py
@@ -15,6 +15,17 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+
+    model = LogisticRegression(eps=1e-5)
+    model.fit(x_train, y_train)
+    
+    # Plot data and decision boundary
+    util.plot(x_train, y_train, model.theta, 'output/p01b_{}.png'.format(pred_path[-5]))
+
+    # Save predictions
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    y_pred = model.predict(x_eval)
+    np.savetxt(pred_path, y_pred > 0.5, fmt='%d')
     # *** END CODE HERE ***
 
 
@@ -35,6 +46,25 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        m, n = x.shape
+        self.theta = np.zeros(n)
+
+        # Newton's method
+        while True:
+            # Save old theta
+            theta_old = np.copy(self.theta)
+            
+            # Compute Hessian Matrix
+            h_x = 1 / (1 + np.exp(-x.dot(self.theta)))
+            H = (x.T * h_x * (1 - h_x)).dot(x) / m
+            gradient_J_theta = x.T.dot(h_x - y) / m
+
+            # Updata theta
+            self.theta -= np.linalg.inv(H).dot(gradient_J_theta)
+
+            # End training
+            if np.linalg.norm(self.theta-theta_old, ord=1) < self.eps:
+                break
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -47,4 +77,5 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return 1 / (1 + np.exp(-x.dot(self.theta)))
         # *** END CODE HERE ***
